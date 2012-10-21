7a2dd431585ac1cd402077b07e10080bdc4f8a8e
Fatos Ismali <fatos.ismali@kcl.ac.uk>
2012-10-21 13:39:05 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personalcv::Application.config.secret_token = 'ad08d8f4ae658f46fe31cf5f8065ab5c6fedb76383530204d9343fda6ad413fad933090b80caf152b7521e53237036f4a2112b22816491d4a7a36705c4f039ba'
